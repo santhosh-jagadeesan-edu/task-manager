36bee00dd05dc3244f81ad448d4d6b9d7507be6b
santhosh-j-devops <santhoshjdevops@gmail.com>
2023-07-08 09:42:16 +0530

    mongodb connection established

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+
 const PORT = process.env.PORT || 3000;
+const connectDB = require('./db/connect');
 
 const tasks = require('./routes/tasks');
-
-app.get('/', (req, res) => res.send('Welcome to Task Manager'));
 
 // Middleware
 app.use(express.json());
 
+app.get('/hello', (req, res) => res.json({ msg: 'Hello!!!' }));
+
+const startApplication = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+
 // Routes
 app.use('/api/v1/tasks', tasks);
 
-app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
+startApplication();
